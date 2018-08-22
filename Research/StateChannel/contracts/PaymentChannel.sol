7cdefeb896450aca85e5163faf74bdad5b9e1070
connorblockchain@protonmail.com <connorblockchain@protonmail.com>
2018-08-22 13:38:26 +0200

    Payment channel Smart Contract added

---
+++ Research/StateChannel/contracts/PaymentChannel.sol
@@ -0,0 +1,54 @@
+pragma solidity ^0.4.24;
+
+contract PaymentChannel {
+
+	address public channelSender;
+	address public channelRecipient;
+	uint public startDate;
+	uint public channelTimeout;
+	mapping (bytes32 => address) signatures;
+
+  constructor(address to, uint timeout) payable {
+		channelRecipient = to;
+		channelSender = msg.sender;
+		startDate = now;
+		channelTimeout = timeout;
+	}
+
+	function CloseChannel(bytes32 h, uint8 v, bytes32 r, bytes32 s, uint value)
+  public {
+
+		address signer;
+		bytes32 proof;
+
+		// get signer from signature
+		signer = ecrecover(h, v, r, s);
+
+		// signature is invalid, throw
+		if (signer != channelSender && signer != channelRecipient) throw;
+
+		proof = keccak256(abi.encodePacked(this, value));
+
+		// signature is valid but doesn't match the data provided
+		if (proof != h) throw;
+
+		if (signatures[proof] == 0)
+			signatures[proof] = signer;
+		else if (signatures[proof] != signer){
+			// channel completed, both signatures provided
+			if (!channelRecipient.send(value)) throw;
+			selfdestruct(channelSender);
+		}
+
+	}
+
+	function ChannelTimeout()
+  public {
+		if (startDate + channelTimeout > now)
+			throw;
+
+		selfdestruct(channelSender);
+	}
+
+  // CREDIT : https://raw.githubusercontent.com/mattdf/payment-channel/master/channel.sol
+}
