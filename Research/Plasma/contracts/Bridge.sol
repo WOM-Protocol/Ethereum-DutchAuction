37fc396a5704cda880f00ad352550d0eb858b4ad
connorblockchain@protonmail.com <connorblockchain@protonmail.com>
2018-08-22 13:27:35 +0200

    added credit bridge

--- Research/Plasma/contracts/Bridge.sol
+++ Research/Plasma/contracts/Bridge.sol
@@ -1,11 +1,3 @@
-
-/**
- * Copyright (c) 2017-present, Parsec Labs (parseclabs.org)
- *
- * This source code is licensed under the Mozilla Public License, version 2,
- * found in the LICENSE file in the root directory of this source tree.
- */
-
 pragma solidity ^0.4.19;
 
 import './Libraries/SafeMath.sol';
@@ -603,7 +595,7 @@ contract Bridge {
   }
 
   /*
-    CREDIT: parseclabs - parseclabs.org
+    CREDIT: Parsec Labs (parseclabs.org)
   */
 
 }
