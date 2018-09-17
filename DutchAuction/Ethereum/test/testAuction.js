956a7ece7c60ddd0603bad39ce01fddd4eb1b27c
connorblockchain@protonmail.com <connorblockchain@protonmail.com>
2018-09-17 16:10:11 +0200

    deleted debugging tests

--- DutchAuction/Ethereum/test/testAuction.js
+++ DutchAuction/Ethereum/test/testAuction.js
@@ -91,75 +91,21 @@ contract('test - auction', function(accounts) {
   });
 
   it('Current price', async () => {
-		console.log('before ', web3.eth.getBlock(web3.eth.blockNumber).timestamp);
+
 		increaseTime(1000);
-		console.log('after1 ', web3.eth.getBlock(web3.eth.blockNumber).timestamp);
+
 
 		increaseTime(HOUR_EPOCH);
-		console.log('hours passed', await auctionInstance.hoursPassed());
 
-
-		var beginTime = await auctionInstance.beginTime();
-		var firstCurrent = await auctionInstance.currentPrice();
-		var inNumber = firstCurrent.toNumber();
-		var usd = await auctionInstance.USDWEI();
-		var usdWEI = usd.toNumber();
-
-
-
-		let line1Valx = web3.eth.getBlock(web3.eth.blockNumber).timestamp;
-		let line2Val1x = usdWEI * 33200;
-		let line2Val2x = usdWEI * 65;
-		let line2Val3x = line1Valx - BEGIN_TIME + 80;
-		let divisorx  = 350;
 		let fValue1 = await auctionInstance.currentPrice();
 		let finaliseValue1 = fValue1.toNumber();
 
-
-		console.log('\n  1 Line ;  \n return (', usdWEI, ' * 33200 / (', line1Valx, ' - ', BEGIN_TIME, ' + 80) - ', usdWEI, ' * 65) / 350');
-		console.log('  2 Line ;  \n return ', line2Val1x, ' /', line2Val3x, '-' , usdWEI * 65,  '/ 350');
-		console.log('  3 Line ;  \n return ', line2Val1x, ' /', line2Val3x - line2Val2x,  '/ 350');
-		console.log('  4 Line ;  \n return ', line2Val1x / (line2Val3x) -  line2Val2x,  '/ 350');
-		console.log('  4 Line ;  \n return ', finaliseValue1);
-
-
-
-		console.log('\n------------------------\n');
 		increaseTime(HOUR_EPOCH*240);
-		console.log('after1 ', web3.eth.getBlock(web3.eth.blockNumber).timestamp);
 
-
-		let line1Val = web3.eth.getBlock(web3.eth.blockNumber).timestamp;
-		var timeDif = parseInt(line1Val) - parseInt(line1Valx);
-		let line2Val1 = usdWEI * 33200;
-		let line2Val2 = usdWEI * 65;
-		let line2Val3 = line1Val - BEGIN_TIME + 80;
-		let divisor  = 350;
 		let fValue = await auctionInstance.currentPrice();
 		let finaliseValue = fValue.toNumber();
 
 
-		console.log('\n  1 Line ;  \n return (', usdWEI, ' * 33200 / (', line1Val, ' - ', BEGIN_TIME, ' + 80) - ', usdWEI, ' * 65) / 350');
-		console.log('  2 Line ;  \n return ', line2Val1, ' /', line2Val3, '-' , usdWEI * 65,  '/ 350');
-		console.log('  3 Line ;  \n return ', line2Val1, ' /', line2Val3 - line2Val2,  '/ 350');
-		console.log('  4 Line ;  \n return ', line2Val1 / (line2Val3) -  line2Val2,  '/ 350');
-		console.log('  4 Line ;  \n return ', finaliseValue);
-
-		let hoursPassed = await auctionInstance.hoursPassed();
-		console.log('>>>>>>>>>>>>>><<<<<<<<<<<<<<');
-		console.log('hours passed; ', hoursPassed.toNumber());
-		console.log('Time Difference seconds; ', timeDif);
-		console.log('Difference in final values', finaliseValue - finaliseValue1);
-
-
-		//   return (USDWEI * 33200 / (now - beginTime + 80) - USDWEI * 65) / 350;
-		//   return (5469741975308642 * 33200 / (1536788156 - 1536787155 + 80) - 5469741975308642 * 65) / 350;
-		//   return (181595433580246925312 / 1081 - 355533228395061760) / 350;
-		//   return (181595433580246925312 / -355533228395060672) / 350;
-		//   return -510.769230769 / 350;
-		//   return -1.459340659
-
-		// WEI == 3.3083454067804627e+74
 	});
 
 });
